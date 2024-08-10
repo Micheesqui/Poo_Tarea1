5fba10cacd59a6ce33006ab61f43d5ee0b359e18
Michelle <sthesqu1@upana.edu.gt>
2024-08-10 17:45:46 -0600

    Agregar Numbo

---
+++ Numbo_151458.py
@@ -0,0 +1,21 @@
+# Numbo
+# Heidy Michelle Esquivel Perez
+# Programación orientada a objetos
+
+## Declaracion de variables
+
+tablero = [0,0,0,0,0,0,
+           0,0,0,0,0,0,
+           0,0,0,0,0,0,
+           0,0,0,0,0,0,
+           0,0,0,0,0,0,
+           0,0,0,0,0,0]
+
+numero1 = " "
+numero2 = " "
+
+hay_ganador = False
+turno_actual = True
+
+print(tablero)
+
